fd541aa86b75f24c732288135ed93fa80dd92bcd
khushi yadav <khushiyadav@khushis-MacBook-Air.local>
2026-06-17 15:17:21 +0530

    uploaded

---
+++ Day5/find_largest_prime_num.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n, largest = 0;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        if (n % i == 0 && isPrime(i))
+            largest = i;
+    }
+
+    cout << "Largest Prime Factor = " << largest;
+
+    return 0;
+}
