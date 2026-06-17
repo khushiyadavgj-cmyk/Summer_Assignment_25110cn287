2fd10b6f791a9e9f182e771cf257628ec3909b18
khushi yadav <khushiyadav@khushis-MacBook-Air.local>
2026-06-17 15:24:00 +0530

    uploaded

---
+++ Day6/count_sets_bits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cin >> n;
+
+    while (n > 0) {
+        count += n & 1;
+        n >>= 1;
+    }
+
+    cout << "Set Bits = " << count;
+
+    return 0;
+}
