8962895ab0e01062056a0e1759b661eb71598c18
khushi yadav <khushiyadav@khushis-MacBook-Air.local>
2026-06-17 13:45:10 +0530

    uploaded

---
+++ Day3/lcm_of_two_num.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, gcd;
+
+    cin >> a >> b;
+
+    int x = a, y = b;
+
+    while(y != 0) {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+
+    gcd = x;
+
+    int lcm = (a * b) / gcd;
+
+    cout << "LCM = " << lcm;
+
+    return 0;
+}
