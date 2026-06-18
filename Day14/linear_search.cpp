3aec4a1d14063b74348349c93235c8e4908c80e1
khushi yadav <khushiyadav@khushis-MacBook-Air.local>
2026-06-18 23:50:38 +0530

    uploaded

---
+++ Day14/linear_search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cin >> key;
+
+    int pos = -1;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            pos = i;
+            break;
+        }
+    }
+
+    if(pos != -1)
+        cout << "Found at index " << pos;
+    else
+        cout << "Not Found";
+
+    return 0;
+}
