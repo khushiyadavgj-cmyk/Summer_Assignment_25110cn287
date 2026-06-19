2552868d9beb0b9c4b8f332162da5e9eca73d93a
khushi yadav <khushiyadav@khushis-MacBook-Air.local>
2026-06-19 15:11:03 +0530

    uploaded

---
+++ Day17/merge_two_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    int arr1[n1], arr2[n2];
+
+    for(int i = 0; i < n1; i++)
+        cin >> arr1[i];
+
+    for(int i = 0; i < n2; i++)
+        cin >> arr2[i];
+
+    for(int i = 0; i < n1; i++)
+        cout << arr1[i] << " ";
+
+    for(int i = 0; i < n2; i++)
+        cout << arr2[i] << " ";
+
+    return 0;
+}
